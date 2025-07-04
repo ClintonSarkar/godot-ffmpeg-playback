f7cf9a24c645846f746d6d395500d4c81845cd06
BarshonClintonSarkar <clinton.sarkar@idnerd.com>
2025-07-04 18:36:31 +0800

    looping videos

--- scripts/ffmpeg.gd
+++ scripts/ffmpeg.gd
@@ -4,6 +4,8 @@ const vid1 = "res://videos/HD Alpha.mov"
 const vid2 = "res://videos/HD Alpha.webm"
 var path = ""
 
+var manual_loop = true
+
 @onready var ffmpeg:FFmpegVideoStream = FFmpegVideoStream.new()
 
 func _ready() -> void:
@@ -12,20 +14,26 @@ func _ready() -> void:
 	stream = ffmpeg
 	return
 	
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Testing"):
 		visible = true
+		manual_loop = true
 		play()
-	if !is_playing():
-		if Input.is_action_just_pressed("Reload"):
-			ffmpeg.file = path
-		if Input.is_action_just_pressed("Switch"):
+		
+	if manual_loop:
+		if get_stream_position() >= 0.2:
+			stop()
 			match ffmpeg.file:
 				vid1:
 					path = vid2
 				vid2:
 					path = vid1
 			ffmpeg.file = path
+			play()
+#
+	if Input.is_action_just_pressed("Stop"):
+		manual_loop = false
+
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
 	return
@@ -33,4 +41,5 @@ func _physics_process(_delta: float) -> void:
 
 func _on_finished() -> void:
 	visible = false
+
 	return
